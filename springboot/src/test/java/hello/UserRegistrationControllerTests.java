b330b8c9b0dd3b9c8fc1b44b16f04b87fd455541
nocox <ddnd7nocox@gmail.com>
2019-07-09 07:55:58 +0900

    エラーハンドリングをテスト項目に追加

--- springboot/src/test/java/hello/UserRegistrationControllerTests.java
+++ springboot/src/test/java/hello/UserRegistrationControllerTests.java
@@ -3,7 +3,6 @@ package hello;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.entity.User;
 import hello.repository.UserRepository;
-import org.apache.catalina.mapper.Mapper;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,10 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -41,29 +38,118 @@ public class UserRegistrationControllerTests {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Test
-    public void userRegistration() throws Exception {
+    private User createTestUser() {
         User testUser = new User();
         testUser.setUsername("hogehoge");
         testUser.setPassword("takatakataka");
         testUser.setPasswordConfirm("takatakataka");
         testUser.setEmail("doi@aaa.com");
         testUser.setDisplay_name("nocox");
+        return testUser;
+    }
+
+    @Test
+    public void userRegistration() throws Exception {
+        User testUser = createTestUser();
 
         this.mockMvc.perform(
-                post("/registration")
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsBytes(testUser))
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
         ).andDo(print()).andExpect(status().isOk());
 
         Optional<User> findUser = userRepository.findByUsername("hogehoge");
         assert findUser.isPresent();
 
-        Assert.assertThat(findUser.get().getUsername() , Is.is(testUser.getUsername()));
+        Assert.assertThat(findUser.get().getUsername(), Is.is(testUser.getUsername()));
         Assert.assertThat(bCryptPasswordEncoder.matches(testUser.getPassword(), findUser.get().getPassword()), Is.is(true));
-        Assert.assertThat(findUser.get().getEmail() , Is.is(testUser.getEmail()));
-        Assert.assertThat(findUser.get().getDisplay_name() , Is.is(testUser.getDisplay_name()));
+        Assert.assertThat(findUser.get().getEmail(), Is.is(testUser.getEmail()));
+        Assert.assertThat(findUser.get().getDisplay_name(), Is.is(testUser.getDisplay_name()));
     }
 
+    @Test
+    public void shortUserNameException() throws Exception {
+
+        User testUser = createTestUser();
+        testUser.setUsername("aa");
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    public void longUserNameException() throws Exception {
+
+        User testUser = createTestUser();
+        testUser.setUsername("abababababababababababababababababababababababababa");
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    public void userAlreadyExistException() throws Exception {
+
+        User testUser = createTestUser();
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().isOk());
+
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    public void shortPasswordException() throws Exception {
+
+        User testUser = createTestUser();
+        testUser.setPassword("aabbc");
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    public void longPasswordException() throws Exception {
+
+        User testUser = createTestUser();
+        testUser.setPassword("aaaaaaaaabbbbbbbbbbbcc");
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
+
+
+    @Test
+    public void passwordNotMatch() throws Exception {
+
+        User testUser = createTestUser();
+        testUser.setPasswordConfirm("aaaaaaaaa");
+
+        this.mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsBytes(testUser))
+        ).andDo(print()).andExpect(status().is5xxServerError());
+    }
 
 }
