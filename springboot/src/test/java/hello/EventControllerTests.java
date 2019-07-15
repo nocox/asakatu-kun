6eadb3895c01615f49bd5cbe5400f6624dc63b73
projectormato <k.keita811.anuy@gmail.com>
2019-07-15 21:52:52 +0900

    [WIP]テストを追加しました

--- springboot/src/test/java/hello/EventControllerTests.java
+++ springboot/src/test/java/hello/EventControllerTests.java
@@ -1,6 +1,8 @@
 package hello;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.entity.Event;
+import hello.entity.UserStatus;
 import hello.repository.EventRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,12 +12,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +35,13 @@ public class EventControllerTests extends AbstractTest{
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+
 	@Override
 	@Before
 	public void setUp() {
@@ -35,7 +49,8 @@ public class EventControllerTests extends AbstractTest{
 	}
 
 	@Test
-	public void getEventsList() throws Exception {
+    @WithMockUser
+    public void getEventsList() throws Exception {
 		String uri = "/events";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
@@ -45,5 +60,21 @@ public class EventControllerTests extends AbstractTest{
 		String content = mvcResult.getResponse().getContentAsString();
 		Event[] eventList = super.mapFromJson(content, Event[].class);
 		assertTrue(eventList.length > 0);
+	}
+
+	private UserStatus joinUserStatus() {
+		UserStatus userStatus = new UserStatus();
+		userStatus.setComment("comment");
+		return userStatus;
+	}
+
+	@Test
+    @WithMockUser
+	public void joinEvent() throws Exception {
+		this.mockMvc.perform(
+				post("/event/1/user")
+						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+						.content(mapper.writeValueAsBytes(joinUserStatus())))
+				.andDo(print()).andExpect(status().isOk());
 	}
 }
