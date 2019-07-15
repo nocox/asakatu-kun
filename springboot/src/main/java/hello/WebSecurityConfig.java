99b2b5a477ae0f2af71da74f92ee37a1268946dc
nocox <ddnd7nocox@gmail.com>
2019-07-15 12:14:39 +0900

    preloginを廃止 (corsを設定，csrfは今回考えない)

--- springboot/src/main/java/hello/WebSecurityConfig.java
+++ springboot/src/main/java/hello/WebSecurityConfig.java
@@ -21,12 +21,16 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .mvcMatchers("/greeting", "/user_registration", "/prelogin" )
+                .mvcMatchers("/greeting", "/user_registration" )
                     .permitAll()
                 .mvcMatchers( "/")
                     .hasRole("USER")
@@ -55,11 +59,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .successHandler(new SimpleAuthenticationSuccessHandler())
                     .failureHandler(new SimpleAuthenticationFailureHandler())
             .and()
+                .cors()
+            .and()
                 .csrf()
-                  .ignoringAntMatchers("/user_registration")
-                  .csrfTokenRepository(new CookieCsrfTokenRepository())
+                    .disable()
         ;
     }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        corsConfiguration.addAllowedOrigin("http://localhost:9000");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", corsConfiguration);
+        return corsSource;
+    }
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth,
