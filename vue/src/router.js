38b4b0381e0a27d64d0023e025725b1461ed83eb
yuki komatsu <yuki.k.lion@gmail.com>
2019-07-20 16:55:35 +0900

    create new vue

--- vue/src/router.js
+++ vue/src/router.js
@@ -18,6 +18,26 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/sign_up',
+      name: 'sign_up',
+      component: () => import('./views/SignUp.vue')
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: () => import('./views/Events.vue')
+    },
+    {
+      path: '/event',
+      name: 'event',
+      component: () => import('./views/Event.vue')
+    },
+    {
+      path: '/event/create',
+      name: 'event_create',
+      component: () => import('./views/EventCreate.vue')
     }
   ]
 })
---
+++ vue/src/views/Events.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="events">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Events"
+    }
+</script>
+
+<style scoped>
+
+</style>
