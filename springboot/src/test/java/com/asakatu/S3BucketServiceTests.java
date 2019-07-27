71dd847ab8484a4c2ec7d1d1bc20e19a68feaf07
nocox <ddnd7nocox@gmail.com>
2019-07-27 21:19:06 +0900

    [WIP] S3とのテスト方法 検討中

---
+++ springboot/src/test/java/com/asakatu/S3BucketServiceTests.java
@@ -0,0 +1,15 @@
+package com.asakatu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class S3BucketServiceTests {
+
+    // TODO: S3にアクセスする必要があるので，テストは省略
+}
