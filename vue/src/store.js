69db60d3c8443bcd7c1ad5953e73ee11b8412a4f
yuki komatsu <yuki.k.lion@gmail.com>
2019-08-04 14:07:40 +0900

    add store items

--- vue/src/store.js
+++ vue/src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const loginState = new Vuex.Store({
   state: {
-
+    isLogin: false,
+    userInfo: {}
   },
   mutations: {
+    initLogin(state){
+      state.loginState = false;
+      state.token='';
+      state.userInfo={}
+    },
+    changeToLogin (state, token, userInfo) {
+      state.loginState = true;
+      state.userInfo = userInfo;
+    },
 
   },
   actions: {
 
-  }
-})
+  },
+  plugins: [createPersistedState({
+    key: 'facelook',
+    paths: ['token','loginState','userinfo','testcount'],
+    storage: window.sessionStorage
+  })]
+});
+
+export default loginState;
