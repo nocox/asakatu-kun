2424ee7a394574a37ec417c44f336acbc6c6453d
yuki komatsu <yuki.k.lion@gmail.com>
2019-08-10 17:36:28 +0900

    create my page component

---
+++ vue/src/views/MyPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="mypage">
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "MyPage"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
