2a322609bab3018d77be24163db441ea94b526d2
TashimaTakumi <bb1015103@gmail.com>
2019-10-18 19:29:27 +0900

    style: イベント作成ページのデザインを作成&実装

--- vue/src/views/EventCreate.vue
+++ vue/src/views/EventCreate.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="event-create">
-        <h2>create event</h2>
+        <h2>イベントを作成</h2>
         <form
                 id="event-create-form"
                 @submit="checkEventCreateForm"
@@ -12,84 +12,91 @@
                 </ul>
             </div>
             <p>
-                <label for="name">event title</label>
+                <label for="name">イベント名
                 <input
                         id="name"
                         v-model="request.eventTitle"
                         type="text"
                         name="name"
                 >
+                </label>
             </p>
-            <p>
-                <label for="startDate">start date</label>
-                <input
-                        id="startDate"
-                        v-model="request.startDate"
-                        type="date"
-                        name="startDate"
-                >
+            <p class="event-create-form__start">
+                <label for="startDate"> 開始日時
+                  <div class="event-create-form--twoColumn">
+                    <input
+                            id="startDate"
+                            v-model="request.startDate"
+                            type="date"
+                            name="startDate"
+                    >
+                    <input
+                            id="startTime"
+                            v-model="startTime"
+                            type="time"
+                            name="startTime"
+                    >
+                  </div>
+
+                </label>
             </p>
-            <p>
-                <label for="startDate">start time</label>
-                <input
-                        id="startTime"
-                        v-model="startTime"
-                        type="time"
-                        name="startTime"
-                >
+            <p class="event-create-form__end">
+                  <label for="endDate">終了日時
+                    <div class="event-create-form--twoColumn">
+                      <input
+                              id="endDate"
+                              v-model="request.endDate"
+                              type="date"
+                              name="endDate"
+                      >
+                      <input
+                              id="endTime"
+                              v-model="endTime"
+                              type="time"
+                              name="endTime"
+                      >
+                    </div>
+                    </label>
             </p>
+
             <p>
-                <label for="endDate">end date</label>
-                <input
-                        id="endDate"
-                        v-model="request.endDate"
-                        type="date"
-                        name="endDate"
-                >
-            </p>
-            <p>
-                <label for="endTime">end time</label>
-                <input
-                        id="endTime"
-                        v-model="endTime"
-                        type="time"
-                        name="endTime"
-                >
-            </p>
-            <p>
-                <label for="address">address</label>
+                <label for="address">開催場所
                 <input
                         id="address"
                         v-model="request.address"
                         type="text"
                         name="address"
                 >
+                </label>
             </p>
             <p>
-                <label for="seatInfo">seat info</label>
+                <label for="seatInfo">席情報
                 <input
                         id="seatInfo"
                         v-model="request.seatInfo"
                         type="text"
                         name="seatInfo"
                 >
+                </label>
             </p>
             <p>
-                <label for="eventDetail">eventDetail</label>
+                <label for="eventDetail">イベント詳細
                 <textarea
                         name="eventDetail"
                         id="eventDetail"
                         cols="30"
                         rows="10"
                         v-model="eventDetail"
-                        placeholder="add event details"
+                        placeholder="イベント内容を入力してください"
                 >
-            </textarea>
+              </textarea>
+              </label>
             </p>
             <input
                     type="submit"
                     value="Submit"
                     id="event_create--submit"
+                    class="primaryButton"
             >
         </form>
     </div>
@@ -155,4 +162,56 @@
 
 <style scoped lang="scss">
     @import "../assets/css/base";
+    @import "../assets/css/button";
+
+    .event-create {
+      margin: 0 16px;
+      margin-bottom: 48px;
+
+      .primaryButton {
+        margin-top: 0;
+      }
+
+      h2 {
+        font-size: 24px;
+        text-align: center;
+      }
+      form {
+        margin: 0 auto;
+      }
+
+      p {
+        margin-top: 1rem;
+
+        label {
+          display: block;
+          font-size: 0.75rem;
+          width:100%;
+        }
+
+        input , textarea {
+          box-sizing: border-box;
+          margin-top: 0.25rem;
+          font-size:1rem;
+          border: 1px solid #FFB100;
+          padding:0.5rem;
+          border-radius: 0.25rem;
+          width:100%;
+        }
+      }
+
+      &-form{
+        &--twoColumn {
+          display: flex;
+          > input {
+            width: 40%;
+            & + input {
+              margin-left: 5%;
+            }
+          }
+        }
+      }
+    }
+
+
 </style>
