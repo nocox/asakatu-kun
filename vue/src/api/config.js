09e19407a3a9bf28a19c45be7b541fa353e84fd9
nocox <ddnd7nocox@gmail.com>
2019-11-15 02:48:12 +0900

    axios設定

--- vue/src/api/config.js
+++ vue/src/api/config.js
@@ -1,15 +1,13 @@
 export const API_URL = process.env.VUE_APP_API_URL_BASE;
 
 import Vue from "vue";
-import axios from "axios";
 import VueAxios from "vue-axios";
+import axios from "axios";
+
+Vue.use(VueAxios, axios);
+Vue.axios.defaults.baseURL = API_URL;
 
 const ApiService = {
-    init() {
-        Vue.use(VueAxios, axios);
-        Vue.axios.defaults.baseURL = API_URL;
-    },
-
     query(resource, params) {
         return Vue.axios.get(resource, params).catch(error => {
             throw new Error(`[RWV] ApiService ${error}`);
