f4df712096cb06ee91bd2fca3008930cf6173ed1
nocox <ddnd7nocox@gmail.com>
2019-11-15 02:34:53 +0900

    _button変更によるバグを修正

--- vue/src/components/common/HelloWorld.vue
+++ vue/src/components/common/HelloWorld.vue
@@ -23,7 +23,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "../assets/css/button";
+@import "../../assets/css/_button";
 h3 {
   margin: 40px 0 0;
 }
