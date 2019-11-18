55c6ede3ee68fcbc07be1fa2fc111d1e72fdb2e4
projectormato <k.keita811.anuy@gmail.com>
2019-11-18 21:03:20 +0900

    feat: 参加ユーザ画像を表示するようにした、イベントをクリックしたらイベント詳細に遷移するようにした

--- vue/src/components/eventList/AllEventList.vue
+++ vue/src/components/eventList/AllEventList.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-      <div v-for="event in eventList" :key="event">
+      <div v-for="event in eventList" :key="event.event.id" @click="showEvent(event.event.id)">
         <p>{{ event.designDate }}</p>
         <p>{{ event.event.address }}</p>
         <p>{{ event.event.eventTitle }}</p>
         <p>{{ event.event.eventDetail }}</p>
-        <div v-for="user in event.event.userStatusList" :key="user">
-          <p>{{user.id}}</p>
+        <div v-for="user in event.joinedUserInfo" :key="user.id">
+          <img :src="user.imagePath">
         </div>
         <p>-----------------------------</p>
       </div>
@@ -19,6 +19,11 @@
         props: {
             eventList: Array
         },
+        methods: {
+          showEvent: function (id) {
+            this.$router.push(`/event/detail/${id}`)
+          }
+        }
     }
 </script>
 
