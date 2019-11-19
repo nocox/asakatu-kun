cb6d7ae566d4010638c69ef95ff77a00f6dbe2b3
projectormato <k.keita811.anuy@gmail.com>
2019-11-20 07:50:04 +0900

    fix: コンポーネントが正しく動いていなかったので切り戻し

--- vue/src/components/eventList/EventList.vue
+++ vue/src/components/eventList/EventList.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="eventList">
-        <div v-for="event in eventList" :key="event.event.id" @click="showEvent(event.event.id)">
-            <router-link to="/event/1">
-                <eventCard/>
-            </router-link>
+    <div>
+      <div v-for="event in eventList" :key="event.event.id" @click="showEvent(event.event.id)">
+        <p>{{ event.designDate }}</p>
+        <p>{{ event.event.address }}</p>
+        <p>{{ event.event.eventTitle }}</p>
+        <p>{{ event.event.eventDetail }}</p>
+        <div v-for="user in event.joinedUserInfo" :key="user.id">
+          <img :src="user.imagePath">
         </div>
+        <p>-----------------------------</p>
+      </div>
     </div>
 </template>
 
